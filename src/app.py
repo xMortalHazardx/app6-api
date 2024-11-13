a507693e92bcfefe71c0e9dbc1d7ffef22fd10fd
xmortalhazardx <cask.overpower_13@hotmail.com>
2024-11-13 17:43:16 -0300

    Atualizando Rest API Flask - 1.3

--- src/app.py
+++ src/app.py
@@ -1,14 +1,51 @@
 import os
+import click
+import sqlalchemy as sa
 
-from flask import Flask
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+from datetime import datetime
+
+
+class Base(DeclarativeBase):
+  pass
+
+
+db = SQLAlchemy(model_class=Base)
+
+
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(unique=True)
+    
+class Post:
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.Datetime, default=sa.func.now())
+    author_id: Mapped[int]= mapped_column(sa.ForeignKey('user.id'))
+    pass
+
+
+@click.command('init-db')
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    global db
+    with current_app.app_context():
+        db.create_all()
+    
+    click.echo('Initialized the database.')
 
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE='diobank.sqlite',
+        SQLALCHEMY_DATABASE_URI='sqlite:///dio_bank.sqlite',
     )
 
     if test_config is None:
@@ -18,12 +55,15 @@ def create_app(test_config=None):
         # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:    
         pass
-    
 
-    from . import db
-
+    # Register cli commands
+    app.cli.add_command(init_db_command)
+    # Initialize extensions
     db.init_app(app)    
+
 
     return app
--- src/db.py
+++ src/db.py
@@ -8,7 +8,7 @@ from flask import current_app, g
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
+            current_app.config['SQLALCHEMY_DATABASE_URI'],
             detect_types=sqlite3.PARSE_DECLTYPES
         )
         g.db.row_factory = sqlite3.Row
@@ -21,6 +21,7 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
 
 def init_db():
     db = get_db()
